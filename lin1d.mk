24819701e961c2ffb451e2ee8cb12f4cac264073
est <est@localhost.localdomain>
2017-03-02 18:26:11 +0100

    Minor changes: move codelite workspace into main folder.  Compile it on Debian x86 linux

--- lin1d.mk
+++ lin1d.mk
@@ -5,18 +5,18 @@
 ## Debug
 ProjectName            :=lin1d
 ConfigurationName      :=Debug
-WorkspacePath          := "E:\txinto\cvs\lin1d\lin1d"
-ProjectPath            := "E:\txinto\cvs\lin1d"
+WorkspacePath          := "/home/est/dev/gatatac/arduino/lin1d"
+ProjectPath            := "/home/est/dev/gatatac/arduino/lin1d"
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
-User                   :=txinto
-Date                   :=02/03/2017
-CodeLitePath           :="C:\Program Files\CodeLite"
-LinkerName             :=C:/soft/cygwin64/bin/x86_64-pc-cygwin-g++.exe
-SharedObjectLinkerName :=C:/soft/cygwin64/bin/x86_64-pc-cygwin-g++.exe -shared -fPIC
+User                   :=est
+Date                   :=03/02/17
+CodeLitePath           :="/home/est/.codelite"
+LinkerName             :=/usr/bin/g++ 
+SharedObjectLinkerName :=/usr/bin/g++ -shared -fPIC
 ObjectSuffix           :=.o
 DependSuffix           :=.o.d
 PreprocessSuffix       :=.i
@@ -34,9 +34,7 @@ ArchiveOutputSwitch    :=
 PreprocessOnlySwitch   :=-E
 ObjectsFileList        :="lin1d.txt"
 PCHCompileFlags        :=
-MakeDirCommand         :=makedir
-RcCmpOptions           := 
-RcCompilerName         :=windres
+MakeDirCommand         :=mkdir -p
 LinkOptions            :=  
 IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
 IncludePCH             := 
@@ -49,19 +47,19 @@ LibPath                := $(LibraryPathSwitch).
 ## Common variables
 ## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
 ##
-AR       := C:/soft/cygwin64/bin/x86_64-pc-cygwin-ar.exe rcu
-CXX      := C:/soft/cygwin64/bin/x86_64-pc-cygwin-g++.exe
-CC       := C:/soft/cygwin64/bin/x86_64-pc-cygwin-gcc.exe
+AR       := /usr/bin/ar rcu
+CXX      := /usr/bin/g++ 
+CC       := /usr/bin/gcc 
 CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
 CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
 ASFLAGS  := 
-AS       := C:/soft/cygwin64/bin/x86_64-pc-cygwin-as.exe
+AS       := /usr/bin/as 
 
 
 ##
 ## User defined environment variables
 ##
-CodeLiteDir:=C:\Program Files\CodeLite
+CodeLiteDir:=/usr/share/codelite
 Objects0=$(IntermediateDirectory)/maincl.cpp$(ObjectSuffix) $(IntermediateDirectory)/DRE.c$(ObjectSuffix) $(IntermediateDirectory)/DummyFunctions.c$(ObjectSuffix) $(IntermediateDirectory)/lin1dFSM.c$(ObjectSuffix) $(IntermediateDirectory)/lin1dFSM_CI.c$(ObjectSuffix) $(IntermediateDirectory)/ProjectFunctions.c$(ObjectSuffix) 
 
 
@@ -71,7 +69,7 @@ Objects=$(Objects0)
 ##
 ## Main Build Targets 
 ##
-.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+.PHONY: all clean PreBuild PrePreBuild PostBuild
 all: $(OutputFile)
 
 $(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
@@ -80,12 +78,8 @@ $(OutputFile): $(IntermediateDirectory)/.d $(Objects)
 	@echo $(Objects0)  > $(ObjectsFileList)
 	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
 
-MakeIntermediateDirs:
-	@$(MakeDirCommand) "./Debug"
-
-
 $(IntermediateDirectory)/.d:
-	@$(MakeDirCommand) "./Debug"
+	@test -d ./Debug || $(MakeDirCommand) ./Debug
 
 PreBuild:
 
@@ -94,7 +88,7 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/maincl.cpp$(ObjectSuffix): maincl.cpp $(IntermediateDirectory)/maincl.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "E:/txinto/cvs/lin1d/maincl.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/maincl.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/est/dev/gatatac/arduino/lin1d/maincl.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/maincl.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/maincl.cpp$(DependSuffix): maincl.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/maincl.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/maincl.cpp$(DependSuffix) -MM "maincl.cpp"
 
@@ -102,7 +96,7 @@ $(IntermediateDirectory)/maincl.cpp$(PreprocessSuffix): maincl.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/maincl.cpp$(PreprocessSuffix) "maincl.cpp"
 
 $(IntermediateDirectory)/DRE.c$(ObjectSuffix): DRE.c $(IntermediateDirectory)/DRE.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "E:/txinto/cvs/lin1d/DRE.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/DRE.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "/home/est/dev/gatatac/arduino/lin1d/DRE.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/DRE.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/DRE.c$(DependSuffix): DRE.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/DRE.c$(ObjectSuffix) -MF$(IntermediateDirectory)/DRE.c$(DependSuffix) -MM "DRE.c"
 
@@ -110,7 +104,7 @@ $(IntermediateDirectory)/DRE.c$(PreprocessSuffix): DRE.c
 	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/DRE.c$(PreprocessSuffix) "DRE.c"
 
 $(IntermediateDirectory)/DummyFunctions.c$(ObjectSuffix): DummyFunctions.c $(IntermediateDirectory)/DummyFunctions.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "E:/txinto/cvs/lin1d/DummyFunctions.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/DummyFunctions.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "/home/est/dev/gatatac/arduino/lin1d/DummyFunctions.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/DummyFunctions.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/DummyFunctions.c$(DependSuffix): DummyFunctions.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/DummyFunctions.c$(ObjectSuffix) -MF$(IntermediateDirectory)/DummyFunctions.c$(DependSuffix) -MM "DummyFunctions.c"
 
@@ -118,7 +112,7 @@ $(IntermediateDirectory)/DummyFunctions.c$(PreprocessSuffix): DummyFunctions.c
 	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/DummyFunctions.c$(PreprocessSuffix) "DummyFunctions.c"
 
 $(IntermediateDirectory)/lin1dFSM.c$(ObjectSuffix): lin1dFSM.c $(IntermediateDirectory)/lin1dFSM.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "E:/txinto/cvs/lin1d/lin1dFSM.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/lin1dFSM.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "/home/est/dev/gatatac/arduino/lin1d/lin1dFSM.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/lin1dFSM.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/lin1dFSM.c$(DependSuffix): lin1dFSM.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/lin1dFSM.c$(ObjectSuffix) -MF$(IntermediateDirectory)/lin1dFSM.c$(DependSuffix) -MM "lin1dFSM.c"
 
@@ -126,7 +120,7 @@ $(IntermediateDirectory)/lin1dFSM.c$(PreprocessSuffix): lin1dFSM.c
 	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/lin1dFSM.c$(PreprocessSuffix) "lin1dFSM.c"
 
 $(IntermediateDirectory)/lin1dFSM_CI.c$(ObjectSuffix): lin1dFSM_CI.c $(IntermediateDirectory)/lin1dFSM_CI.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "E:/txinto/cvs/lin1d/lin1dFSM_CI.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/lin1dFSM_CI.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "/home/est/dev/gatatac/arduino/lin1d/lin1dFSM_CI.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/lin1dFSM_CI.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/lin1dFSM_CI.c$(DependSuffix): lin1dFSM_CI.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/lin1dFSM_CI.c$(ObjectSuffix) -MF$(IntermediateDirectory)/lin1dFSM_CI.c$(DependSuffix) -MM "lin1dFSM_CI.c"
 
@@ -134,7 +128,7 @@ $(IntermediateDirectory)/lin1dFSM_CI.c$(PreprocessSuffix): lin1dFSM_CI.c
 	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/lin1dFSM_CI.c$(PreprocessSuffix) "lin1dFSM_CI.c"
 
 $(IntermediateDirectory)/ProjectFunctions.c$(ObjectSuffix): ProjectFunctions.c $(IntermediateDirectory)/ProjectFunctions.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "E:/txinto/cvs/lin1d/ProjectFunctions.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/ProjectFunctions.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "/home/est/dev/gatatac/arduino/lin1d/ProjectFunctions.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/ProjectFunctions.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/ProjectFunctions.c$(DependSuffix): ProjectFunctions.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/ProjectFunctions.c$(ObjectSuffix) -MF$(IntermediateDirectory)/ProjectFunctions.c$(DependSuffix) -MM "ProjectFunctions.c"
 
@@ -147,6 +141,9 @@ $(IntermediateDirectory)/ProjectFunctions.c$(PreprocessSuffix): ProjectFunctions
 ## Clean
 ##
 clean:
-	$(RM) -r ./Debug/
+	$(RM) ./Debug/*$(ObjectSuffix)
+	$(RM) ./Debug/*$(DependSuffix)
+	$(RM) $(OutputFile)
+	$(RM) ".build-debug/lin1d"
 
 
